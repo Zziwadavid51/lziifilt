56835556c0398c4ee68de3208a1746d15090676b
Zziwadavid51 <149446130+Zziwadavid51@users.noreply.github.com>
2024-05-30 09:22:46 -0400

    Update zzz_ai_LazyDeveloper.py
    api rename

--- plugins/zzz_ai_LazyDeveloper.py
+++ plugins/zzz_ai_LazyDeveloper.py
@@ -17,7 +17,7 @@ async def lazy_answer(client, message):
                 parameters = {
                     'question': lazy_users_message,
                 }
-                response = requests.get("https://chatgpt.apinepdev.workers.dev", params=parameters)
+                response = requests.get(GPTAPI, params=parameters)
                 # response = openai.Completion.create(
                 #     model = "text-davinci-003",
                 #     prompt = lazy_users_message,
