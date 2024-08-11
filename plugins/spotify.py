59606885872984ab6594257392a43a8750004ce3
Mr-Righteousdev <mrrighteousdev@gmail.com>
2024-08-11 19:37:00 +0300

    spoty1

---
+++ plugins/spotify.py
@@ -0,0 +1,101 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import aiohttp
+import random
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+CLIENT_ID = "adb3797e3a6a431f9544c6c51f088d9e"
+CLIENT_SECRET = "fdfad8cce09c472f97bde3e2ca7e23bd"
+
+# print("Welcome to the musical time machine playlister")
+
+
+# date = input("Which date do you prefer? ")
+# month = input("Enter yr favourite month? ")
+# year = input("Enter your year of choice ... ")
+# pl_name = input("Enter a playlist name ... ")
+
+# full_date = f"{int(year)}-{int(month)}-{int(date)}"
+# url = f"https://www.billboard.com/charts/hot-100/{full_date}/"
+# url = "https://www.billboard.com/charts/hot-100/1979-08-12/"
+
+# webb = requests.get(url)
+# web = webb.text
+
+
+@Client.on_message(filters.command(['top_100', 'tops', 'top']))
+async def send_toptier(client, message):
+    playlist_names = ['walter','yellow',                    
+    "Happy Vibes Mix",
+    "Chill Beats Collection",
+    "Sunset Serenade",
+    "Summer Breeze Jams",
+    "Starry Night Melodies",
+    "Weekend Chillout Mix",
+    "Feel Good Anthems",
+    "Smooth Sailing Sounds",
+    "Cozy Fireplace Tunes",
+    "Morning Dew Playlist"
+]
+    pl_name = random.choice(playlist_names)
+    date_string = message.text.split(' ', 1)[1]
+    day, month, year = date_string.split('/')
+    full_date = f"{int(year)}-{int(month):02d}-{int(day):02d}"
+    try:
+        # Fetch the webpage content asynchronously
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://www.billboard.com/charts/hot-100/{full_date}/") as response:
+                web_page = await response.text()
+
+        soup = BeautifulSoup(web_page, "html.parser")
+        song_names_spans = soup.select("li ul li h3")
+        song_names = [song.getText().strip() for song in song_names_spans]
+        print(song_names)
+        tracks_to_add = []
+
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                    client_secret=CLIENT_SECRET,
+                                                    redirect_uri="https://example.com",
+                                                    scope="user-library-read playlist-modify-public"))
+
+        pl1 = sp.user_playlist_create(user="31gwwiu2onm2fgqrju62vlsi4a5i", 
+                                name=f"{pl_name} - {full_date}", 
+                                public=True, 
+                                collaborative=False, 
+                                description='This is python-made ... #$%!')
+
+        playlist_id = pl1['id']
+        playlist_location = pl1['external_urls']['spotify']
+
+        for song_name in song_names:
+            results = sp.search(q=f"track:{song_name}", type='track', limit=1)
+            try:
+                song_id = results['tracks']['items'][-1]['id']
+                track = sp.track(track_id=song_id)
+                tracks_to_add.append(song_id)
+                print(track)
+            except IndexError as e:
+                print(f"{song_name} is not available on spotify")
+
+        if tracks_to_add:        
+            sp.user_playlist_add_tracks(user="31gwwiu2onm2fgqrju62vlsi4a5i",playlist_id=f"{playlist_id}",tracks=tracks_to_add)
+
+        print(f"PLAYLIST - {pl_name} created successfully.\nYou can find it at URL : {playlist_location}")
+
+        await client.send_message(
+            chat_id=message.chat.id,
+            text=f"PLAYLIST - {pl_name} created successfully.\nYou can find it at URL : {playlist_location}",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(text='GO', url=playlist_location)],
+                [InlineKeyboardButton(text='Back', url='https://mr-righteousdev.github.io')]
+            ])
+        )
+
+    except Exception as e:
+        print(e)
+
+
+
